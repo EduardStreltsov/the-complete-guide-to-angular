7274a8a4688541a9b3a603b2940a7ca931a73930
eduard.streltsov <streltsov.eduard@gmail.com>
2018-06-13 19:46:51 +0300

    Initial commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  private allowNewServer = false;
+  private serverCreationStatus = 'no server was created!';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created!';
+  }
+
+  onUpdateServerName(event) {
+    console.log(event);
+  }
+
+}
